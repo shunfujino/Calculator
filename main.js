88b54bf63f7891edf451b4f2760fba46bbfef904
EC2 Default User <ec2-user@ip-172-31-28-163.us-east-2.compute.internal>
2021-07-22 11:21:11 +0000

    先頭が0の数字にならないよう修正。小数点の重複を修正。先頭に（/,*,.）を入力できないように修正。

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
 let result = document.getElementById("result");
 let num = ""; // 数字を記憶
-let operator = ""; // 演算子を記憶
 let count = 0; // get_calc()の直後は1
 let ope_on = 0; //最後に入力したのが演算子の場合は1
+let dot_on = 0; //最後に入力したのが小数点の場合は1
+let ans = "1.1";
 
 //消去
 function clear_calc() {
     result.value = "";
     num = "";
-    operator = "";
     count = 0;
+    ope_on = 0;
+    dot_on = 0;
 }
 
 // 計算結果を表示
 function get_calc() {
     result.value = new Function("return " + result.value)();
-    num = "";
+    ans = "" + result.value;
     count = 1;
     ope_on = 0;
+    dot_on = 0;
 }
 
 //数字を入力
@@ -25,89 +28,109 @@ function edit_calc(btn) {
     if (count == 1) {
         num = btn.value;
         count = 0;
-        if(btn.value=="00"){
-            result.value="0";
-        }else{
+        if (btn.value == "00") {
+            result.value = "0";
+        }
+        else {
             result.value = num;
-        
+
         }
         return;
     }
-    
-    else if (num == "0" || num == "00" || num == "") {
+
+    if (result.value == "0" || result.value == "") {
         if (btn.value == "0" || btn.value == "00") {
-            if(ope_on==1){
+            result.value = "0";
+            num = "0";
+            return;
+        }
+    }
+
+    if (num == "0" || num == "00") {
+        if (btn.value == "0" || btn.value == "00") {
+            if (ope_on == 1) {
                 return;
             }
             num = "0";
+            result.value = 0;
+            return;
+        }
+
+        num = "" + btn.value;
+        if (ope_on == 1) {
+            result.value += num;
         }
         else {
-            num = "" + btn.value;
-
+            result.value = num;
         }
-        result.value += num;
         count = 0;
         ope_on = 0;
         return;
     }
-    
+
     else {
         num += btn.value;
         count = 0;
     }
 
     result.value += btn.value;
-    operator = "";
-    ope_on=0;
+    ope_on = 0;
 }
 
-//演算子を入力
-function ope_calc(btn) {
-    if (ope_on==1) {
+//(+,-)を入力
+function plus_minus_calc(btn) {
+    if (ope_on == 1 || result.value.slice(-1) == ".") {
         result.value = result.value.slice(0, -1) + btn.value;
     }
     else {
         result.value += btn.value;
 
     }
-    operator = "" + btn.value;
     count = 0;
     ope_on = 1;
+    dot_on = 0;
+    num = "";
+}
+
+//(*,/)を入力
+function ope_calc(btn) {
+    if (result.value == "" || result.value == "+" || result.value == "-") {
+        return;
+    }
+    if (ope_on == 1 || result.value.slice(-1) == ".") {
+        result.value = result.value.slice(0, -1) + btn.value;
+    }
+    else {
+        result.value += btn.value;
+    }
+    count = 0;
+    ope_on = 1;
+    dot_on = 0;
+    num = "";
 
 }
 
 //小数点を入力
 function dot_calc(btn) {
-    if(result.value.slice(-1)!="."){
-        result.value +=".";
+    if(result.value==""){
+        result.value+="0.";
+        dot_on =1;
+        return;
     }
     
+    if(count==1)
+        if(!Number.isInteger(result.value)){
+        return;
+    }
+
+    if (dot_on == 1 || ope_on == 1) {
+        return;
+    }
+    if (result.value.slice(-1) != ".") {
+        result.value += ".";
+        dot_on = 1;
+        num += ".";
+    }
+    count = 0;
+    ope_on = 0;
 }
-
-
-// function edit_calc(btn) {
-//     if (!isNaN(btn.value)) { //数字を入力
-//         if (operator == 0) {
-//             if (num == "0") {
-//                 if (btn.value == "0" || btn.value == "00") {
-//                     result.value = "0";
-//                     num == "0";
-//                 }
-//                 else {
-//                     result.value = btn.value;
-//                     num = "" + result.value;
-//                 }
-//             }
-//             else {
-//                 result.value += btn.value;
-//                 num += "" + btn.value;
-//             }
-//         }
-//         else {}
-//     }
-//     else { // 数字以外を入力
-//         result.value += btn.value;
-//         operator = 1;
-//     }
-
-// }
